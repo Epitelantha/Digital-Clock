1d86edefafac1f0ec343612b23308cc61bd325ea
Holger <horol18@student.sdu.dk>
2019-03-06 14:29:28 +0100

    It works

--- Digital Clock/Digital Clock/Program.cs
+++ Digital Clock/Digital Clock/Program.cs
@@ -10,14 +10,18 @@ namespace Digital_Clock
     {
         static void Main(string[] args)
         {
-
+            ClockDisplay clock = new ClockDisplay(23, 59);
+            Console.WriteLine(clock.display);
+            clock.TimeTick();
+            Console.WriteLine(clock.display);
+            Console.ReadLine();
         }
     }
 
     public class NumberDisplay
     {
         private int limit;
-        private int value;
+        public int value;
 
         public NumberDisplay(int rolloverLimit)
         {
@@ -54,16 +58,17 @@ namespace Digital_Clock
     public class ClockDisplay
     {
         NumberDisplay hours;
-
         NumberDisplay minutes;
-        NumberDisplay seconds;
+        //NumberDisplay seconds;
+
+        public string display;
 
         public ClockDisplay()
         {
             hours = new NumberDisplay(24);
             minutes = new NumberDisplay(60);
-            seconds = new NumberDisplay(60);
-            //updateDisplay();
+            //seconds = new NumberDisplay(60);
+            UpdateDisplay();
         }
 
         public ClockDisplay(int hour, int minute)
@@ -71,12 +76,28 @@ namespace Digital_Clock
             hours = new NumberDisplay(24);
             minutes = new NumberDisplay(60);
             SetTime(hour, minute);
+            UpdateDisplay();
         }
 
-        void SetTime(int hour, int minute)
+        public void SetTime(int hour, int minute)
         {
             hours.setDisplayValue(hour);
             minutes.setDisplayValue(minute);
+        }
+
+        public void TimeTick()
+        {
+            minutes.increment();
+            if (minutes.value == 0)
+            {
+                hours.increment();
+            }
+            UpdateDisplay();
+        }
+
+        public void UpdateDisplay()
+        {
+            display = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
         }
     }
 }
